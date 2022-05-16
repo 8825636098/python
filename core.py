369267249542e552ded92d023b8b9bb53dd2e05b
8825636098 <101411679+8825636098@users.noreply.github.com>
2022-05-16 22:52:32 +0530

    Update core.py

--- core.py
+++ core.py
@@ -5,4 +5,4 @@ bot_token = "5149667365:AAHD-ATneazqafldnGJJ4rYwJQkrI1_KKBA"
 admins = [2131044278,1080680449]
 
 #MongoDb Url For Databse
-mongo_url = "Mongo Url"
+mongo_url = "mongodb+srv://Data1:Data1@cluster0.ymfdb.mongodb.net/?retryWrites=true&w=majority"
