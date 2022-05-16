de9225667aa897310aa65da79cfa3f7235184494
8825636098 <101411679+8825636098@users.noreply.github.com>
2022-05-16 22:46:34 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,8 @@
+#Bot Token From @BotFather
+bot_token = "5149667365:AAHD-ATneazqafldnGJJ4rYwJQkrI1_KKBA"
+
+#Admins Telegram ID
+admins = [2131044278,1080680449]
+
+#MongoDb Url For Databse
+mongo_url = "Mongo Url"
